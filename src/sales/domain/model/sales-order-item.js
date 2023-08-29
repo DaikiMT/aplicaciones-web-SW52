98416d7aa2c0457bc26185000148a708f4417687
Adrian Melgar <DaikiMT@outlook.es>
2023-08-28 20:34:24 -0500

    added sales order aggregate

---
+++ src/sales/domain/model/sales-order-item.js
@@ -0,0 +1,12 @@
+class SalesOrderItem {
+	constructor({ orderId, productId, quantity, unitPrice }) {
+		this._orderId = orderId;
+		this._productId = productId;
+		this._quantity = quantity;
+		this._unitProice = unitPrice;
+	}
+
+	calculateItemPrice() {
+		return this._unitPrice * this._quantity;
+	}
+}
